d5428d8df24297b572ae31f7999404262194b0e5
h4hardikonly <h4hardikonly@gmail.com>
2017-10-29 02:51:45 +0530

    init commit

---
+++ lib/nse/version.rb
@@ -0,0 +1,3 @@
+module Nse
+  VERSION = "0.1.0"
+end
---
+++ spec/nse_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Nse do
+  it "has a version number" do
+    expect(Nse::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
